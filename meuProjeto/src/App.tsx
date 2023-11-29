49c3d3e75933e14088bff58a83ed69e318519ff7
Unnamed <estudo.zzz@gmail.com>
2023-11-29 03:11:39 -0300

    meuProjeto

---
+++ meuProjeto/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {SafeAreaView, Text} from "react-native"
+import Login from "./modules";
+
+const App = () =>{
+    return(
+        <SafeAreaView>
+            <Login nome={`Login`}></Login>
+
+        </SafeAreaView>
+
+    )
+}
+
+export default App;
